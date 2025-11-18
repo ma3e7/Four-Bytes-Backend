1d3d5e8996b416ea3c7fdf3b6f7a46b01f360735
Vasilije Spalevic <waka4business@gmail.com>
2025-11-18 09:52:26 +0100

    userfiles done
    user and middleware created

---
+++ models/UserModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user", "guest"],
+  }
+});
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    delete returnedObject.password;
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
